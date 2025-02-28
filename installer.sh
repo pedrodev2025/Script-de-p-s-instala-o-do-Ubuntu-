bb80e2303f41926f363c7dd2863f3060ccba8a22
pedrodev2025 <pedrolucasinvestidor.github@gmail.com>
2025-02-28 09:58:46 -0300

    Corrigindo erro de sintaxe

--- installer.sh
+++ installer.sh
@@ -1,24 +1,24 @@
-echo ("A Instalação Está Começando Por Favor Espere")
+echo A Instalação Está Começando Por Favor Espere
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install curl -y
 sudo apt install wget -y
 sudo apt install unzip -y
-echo ("Instalando Snapd")
+echo Instalando Snapd
 sudo apt install snapd -y
-echo ("Instalando Flatpak")
+echo Instalando Flatpak
 sudo apt install flatpak -y
-echo ("Instalando Chrome")
+echo Instalando Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y google-chrome-stable_current_amd64.deb
-echo ("Instalando Flathub")
+echo Instalando Flathub
  flatpak remote-add -y --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-echo ("Instalando VLC")
+echo Instalando VLC
  flatpak install -y flathub org.videolan.VLC
-echo ("Instalando gimp e photo-gimp")
+echo Instalando gimp e photo-gimp
  flatpak install -y flathub org.gimp.GIMP
 wget https://github.com/Diolinux/PhotoGIMP/releases/download/1.1/PhotoGIMP.zip
 sudo unzip PhotoGIMP.zip -d ~/
-echo ("Instalando Audacity")
+echo Instalando Audacity
  flatpak install -y flathub org.audacityteam.Audacity
