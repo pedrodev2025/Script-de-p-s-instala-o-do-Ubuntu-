7c4350a44f68b4202fd496ffcb0d975624d9a366
pedrodev2025 <pedrolucasinvestidor.github@gmail.com>
2025-02-28 09:39:59 -0300

    Script

---
+++ installer.sh
@@ -0,0 +1,24 @@
+echo ("A Instalação Está Começando Por Favor Espere")
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install curl -y
+sudo apt install wget -y
+sudo apt install unzip -y
+echo ("Instalando Snapd")
+sudo apt install snapd -y
+echo ("Instalando Flatpak")
+sudo apt install flatpak -y
+echo ("Instalando Chrome")
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y google-chrome-stable_current_amd64.deb
+echo ("Instalando Flathub")
+ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+echo ("Instalando VLC")
+ flatpak install flathub org.videolan.VLC
+echo ("Instalando gimp e photo-gimp")
+ flatpak install flathub org.gimp.GIMP
+wget https://github.com/Diolinux/PhotoGIMP/releases/download/1.1/PhotoGIMP.zip
+sudo unzip PhotoGIMP.zip -d ~/
+echo ("Instalando Audacity")
+ flatpak install flathub org.audacityteam.Audacity
