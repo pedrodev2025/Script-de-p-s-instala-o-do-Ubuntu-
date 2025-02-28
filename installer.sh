df628b4ecc2446eb567d45c0a8be9517cecb6686
pedrodev2025 <pedrolucasinvestidor.github@gmail.com>
2025-02-28 11:30:22 -0300

    Corrigindo erros

--- installer.sh
+++ installer.sh
@@ -12,7 +12,7 @@ echo Instalando Flatpak
 sudo apt install flatpak -y
 echo Instalando Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install -y google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
 echo Instalando Flathub
  flatpak remote-add  --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
